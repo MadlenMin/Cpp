96dc3b587856a2b355a0c6cbc26fea8d347eb613
MadlenMin <i23.Minasyan.Madlen@etud.ufar.am>
2026-05-27 18:40:54 +0400

    c01/ex04 changed

--- c01/ex04/main.cpp
+++ c01/ex04/main.cpp
@@ -1,38 +1,45 @@
 #include "Replace.hpp"
-#include <sstream>
 
 int main(int argc, char **argv)
 {
     if (argc != 4)
     {
-       std::cout << "Wrong number of arguments "<< '\n';
-	   std::cout << argv[0] <<"<filname> <old_str> <new_str>" << std::endl;
+        std::cout << "Wrong number of arguments " << '\n';
+        std::cout << argv[0] << " <filename> <old_str> <new_str>" << std::endl;
         return 1;
     }
-	std::string filname;
-	std::string oldStr;
-	std::string newStr;
-	filname = argv[1];
-	oldStr = argv[2];
-	newStr = argv[3];
 
-	std::ifstream infile(filname.c_str());
-	if (!infile.is_open())
-	{
-   	 	std::cerr << "Error: Could not open file" << std::endl;
-   	 	return 1;
-	}
-	std::ofstream outfile((filname + ".replace").c_str());
-	 if (!outfile.is_open()) {
-        std::cerr << "Error: could not create output file\n";
+    std::string filename = argv[1];
+    std::string oldStr   = argv[2];
+    std::string newStr   = argv[3];
+
+    std::ifstream infile(filename.c_str());
+    if (!infile.is_open())
+    {
+        std::cerr << "Error: Could not open file" << std::endl;
         return 1;
     }
-	std::stringstream buffer;
-    buffer << infile.rdbuf();
-    std::string content = buffer.str();
-	infile.close();
-	std::string processed = replace_all(content, oldStr, newStr);
-	outfile << processed;
-	outfile.close();
-	return 0;
+
+    std::ofstream outfile((filename + ".replace").c_str());
+    if (!outfile.is_open())
+    {
+        std::cerr << "Error: could not create output file" << std::endl;
+        return 1;
+    }
+
+    std::string content;
+    std::string line;
+    while (std::getline(infile, line))
+    {
+        content += line;
+        if (!infile.eof())
+            content += '\n';
+    }
+    infile.close();
+
+    std::string processed = replace_all(content, oldStr, newStr);
+    outfile << processed;
+    outfile.close();
+
+    return 0;
 }
